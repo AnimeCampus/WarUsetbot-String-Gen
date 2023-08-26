b1cc876ad6dce5c8c0523d0eb9b6508f16347ae4
Ishikki Akabane <70576152+ishikki-akabane@users.noreply.github.com>
2023-08-26 18:25:52 +0530

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,23 @@
+# Made by @iShiKKi-AKABANE
+
+from pyrogram import Client
+import os
+
+api_id = int(os.environ.get("API_ID"))  # put your API_ID value in environment
+api_hash = os.environ.get("API_HASH")  # put your API_HASH value in environment
+
+app = Client("my_account")
+
+
+async def main():
+    await app.start()
+    await app.send_message(
+        "me",
+        "**Pyrogram Session String**:\n\n"
+        f"`{await app.export_session_string()}`"
+    )
+    await app.stop()
+
+
+app.run(main())
+
